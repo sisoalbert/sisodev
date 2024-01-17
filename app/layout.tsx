e48c079eb23c838595536546f19ec2f831ad0a93
Siso Ngqolosi <sisoalbert@yahoo.com>
2024-01-17 23:29:25 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const firebaseAnalytics = getAnalytics();
+  logEvent(firebaseAnalytics, "notification_received");
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
