d6259e577bf960ce67da1578de6567cab822c508
Siso Ngqolosi <sisoalbert@yahoo.com>
2024-02-03 16:00:52 +0200

    nav drawer fix

--- components/ui/navmenubutton.tsx
+++ components/ui/navmenubutton.tsx
@@ -1,16 +1,30 @@
 "use client";
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import clsx from "clsx";
 
 export default function NavMenuButton() {
+  const router = useRouter();
+  const [menuVisible, setMenuVisible] = useState(false);
+
   const toggleMenu = () => {
-    // setMenuVisible(!menuVisible);
-    console.log("toggleMenu");
+    setMenuVisible(!menuVisible);
+    router.push(`?modal=${menuVisible}`);
+    console.log("menuVisible", menuVisible);
   };
+  const searchParams = useSearchParams();
+  const isModalOpen = searchParams.get("modal") === "true";
+
   return (
     <div className="block lg:hidden">
       <button
         onClick={toggleMenu}
-        className="flex items-center px-3 py-2 border rounded text-slate-50 hover:border-transparent hover:text-black hover:bg-white"
+        className={clsx(
+          "flex items-center px-3 py-2 border rounded text-slate-50 hover:border-transparent hover:text-black hover:bg-white",
+          {
+            "text-black bg-white": isModalOpen,
+          }
+        )}
       >
         <svg
           className="fill-current h-3 w-3"
--- components/ui/unprotectednav.tsx
+++ components/ui/unprotectednav.tsx
@@ -5,22 +5,18 @@ import NavMenuButton from "./navmenubutton";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { Database } from "@/database.types";
+import NavMenuList from "./navmenulist";
 
-export default async function UnprotectedNav() {
+export default async function UnprotectedNav({
+  searchParams,
+}: {
+  searchParams: { q: string };
+}) {
   const supabase = createServerComponentClient<Database>({ cookies });
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  // const pathname = usePathname();
-  // const [menuVisible, setMenuVisible] = useState(false);
-
-  // const toggleMenu = () => {
-  //   setMenuVisible(!menuVisible);
-  // };
-
-  const menuVisible = false;
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-black p-6">
@@ -34,55 +30,7 @@ export default async function UnprotectedNav() {
         </span>
       </Link>
       <NavMenuButton />
-      <div
-        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
-          menuVisible ? "block" : "hidden"
-        }`}
-      >
-        <div className="text-sm lg:flex-grow">
-          <Link
-            href="/codelabs"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-500 mr-4"
-          >
-            Codelabs
-          </Link>
-          <Link
-            href="https://www.youtube.com/@questerstudio"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-500 mr-4"
-          >
-            Youtube
-          </Link>
-          <Link
-            href="/blogs"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-500"
-          >
-            Blogs
-          </Link>
-        </div>
-        {!user ? (
-          <div>
-            <Link
-              href="/signup"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-500 pr-4"
-            >
-              Sign up
-            </Link>
-            <Link
-              href="/login"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0"
-            >
-              Login
-            </Link>
-          </div>
-        ) : (
-          <Link
-            href="/account"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-500 mr-4"
-          >
-            {user?.email}
-          </Link>
-        )}
-      </div>
+      <NavMenuList user={user} />
     </nav>
   );
 }
